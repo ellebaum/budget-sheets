895e1e824e7936f9383fb1c0fc526471c14f6445
ellebaum <53582969+ellebaum@users.noreply.github.com>
2020-08-05 10:07:11 -0500

    Update spreadsheet_tools.rb

--- spreadsheet_tools.rb
+++ spreadsheet_tools.rb
@@ -5,35 +5,34 @@ def generate_actuals_yaml (input_path, output_path)
 	path = input_path
 	book = RubyXL::Parser.parse(path)
 	actual_center_hash = YAML.load(File.read(output_path))
-	puts actual_center_hash
+
 	budget_sheet = book[0]
 	#####Budget Info##############################
 	#SIM_Cost Center  -- Column C [2]
 	#SIM_GL           -- Column E [4]
 	#Actual(do later) -- Column G [6]
+
 	actual_sheet = book['WF Actuals']
 	#####Actual Info##############################
 	#Cost Center      -- Column B [1]
+	#Account Text     -- Column p [15]
 	#Account Number   -- Column E [4]
+	#Vendor Name      -- Column AC [28]
+	#Vendor Number    -- Column R [17]
 	#Currency         -- Column M [12]
+
 	########Underlying data structure#############
 	#actual_center_hash = {
-	#		"account_1" => [100, 200, 300, 400],
 	#	"center_1" => {
-	#		"account_2" => [500, 200000]
+	#		"account_2 + acc_2_number" => {
+	#			"vendor_2_name + vendor_2_number" => [500, 200000]
+	#		}
 	#	},
-	#	"center_2" => {
-	#		"account_1" => [9999, 6000, 25],
-	#		"account_50" => [5, 90, 450]
-	#	},
-	#	"center_3" => {
-	#		"account_2" => [670, 890, 2000],
-	#		"account_35" => [299, 68001, 8]
-	#	}
 	#}
 	#i.e., a hash of hashes of arrays
+
 	if !actual_center_hash #if hash doesn't exist (why does it work this way?)
-		actual_center_hash = Hash.new{|hsh,key| hsh[key] = Hash.new{|hsh2,key2| hsh2[key2] = []}}
+		actual_center_hash = Hash.new{|hsh, key| hsh[key] = Hash.new{|hsh2, key2| hsh2[key2] = Hash.new{|hsh3, key3| hsh3[key3] = []}}}
 	else
 		old_actuals_hash = actual_center_hash.dup #to eliminate duplicate entries
 	end
@@ -44,15 +43,33 @@ def generate_actuals_yaml (input_path, output_path)
 			end
 			cost_center    = actual_sheet[index][1].value  #Column B
 			account_number = actual_sheet[index][4].value  #Column E
+			account_text   = actual_sheet[index][15].value #Column P
+			vendor_name    = actual_sheet[index][28].value #Column AC
+			vendor_number  = actual_sheet[index][17].value #Column R
 			currency       = actual_sheet[index][12].value #Column M
 			#Default Cost Center is 37000000 if field is blank
 			if cost_center.nil?
 				cost_center = '37000000'
 			end
-			actual_center_hash[cost_center][account_number].push(currency)
+			if vendor_name == ""
+				vendor_name = "NO VENDOR NAME"
+			end
+			if vendor_number == ""
+				vendor_number = "NO VENDOR NUMBER"
+			end
+			actual_center_hash[cost_center.to_s][account_text.to_s + " " + account_number.to_s][vendor_name.to_s + " " + vendor_number.to_s].push(currency)
 		end
 	end
 	File.open(output_path, "w") { |file| file.write(actual_center_hash.to_yaml)}
 end
 
-generate_actuals_yaml('./sample.xlsx', 'actuals.yaml')
+def yaml_to_sqlite (yaml_file)
+	hash1 = YAML.load_file(yaml_file)
+	puts hash1.to_s
+
+end
+
+
+
+#generate_actuals_yaml('./2020_RF_EAST_HW_06.xlsx', 'actuals.yaml')
+yaml_to_sqlite('./actuals.yaml')
